95984b520ba5818fb50b62297f30587299c681b0
Avinash Mandal <avinashmandal0731@gmail.com>
2026-03-15 23:48:13 +0530

    Add files via upload

---
+++ Day05_understanding_potentionmeter/understanding_potentionmeter.ino
@@ -0,0 +1,18 @@
+int mypin = A1;
+int readVal ;
+float V2 ;
+int DelayT = 250;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readVal = analogRead(mypin);
+  V2 = (5./1023.)*readVal;
+  Serial.println(V2);
+
+  delay(DelayT);
+}
